226c2c3cbcb1d4e3812ae3ede44756da1517eb23
PassosGabriel <gabriel_silvapassos@hotmail.com>
2022-05-24 18:28:57 -0300

    Validation

--- SaleWebMvc/Models/Seller.cs
+++ SaleWebMvc/Models/Seller.cs
@@ -8,14 +8,20 @@ namespace SaleWebMvc.Models
     public class Seller
     {
         public int Id { get; set; }
+        [Required(ErrorMessage ="{0} Required")]
+        [StringLength(60, MinimumLength = 3, ErrorMessage ="{0} Name size should be between {2} and {1}")]
         public string Name { get; set; }
+        [Required(ErrorMessage = "{0} Required")]
         [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
+        [Required(ErrorMessage = "{0} Required")]
         [Display(Name = "Birth Date")]
         [DataType(DataType.Date)]
-        ´[DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime Date { get; set; }
         [DisplayFormat(DataFormatString = "{0:F2}")]
+        [Required(ErrorMessage = "{0} Required")]
+        [Range(100,50000, ErrorMessage ="{0} must be from {1} to {2}")]
         public double BaseSalary { get; set; }
         public Department Department { get; set; }
         public int DepartmentId { get; set; }
