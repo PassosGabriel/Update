8a791e29ba40f9083518bafea1c3813ea6362713
PassosGabriel <gabriel_silvapassos@hotmail.com>
2022-05-23 21:42:47 -0300

    SelleForm bla bla bla

--- SaleWebMvc/Controllers/SellersController.cs
+++ SaleWebMvc/Controllers/SellersController.cs
@@ -1,16 +1,18 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using SaleWebMvc.Services;
 using SaleWebMvc.Models;
+using SaleWebMvc.Models.ViewModels;
 
 namespace SaleWebMvc.Controllers
 {
     public class SellersController : Controller
     {
         private readonly SellerService _sellerService;
-
-        public SellersController(SellerService sellerService)
+        private readonly DepartmentService _departmentService;
+        public SellersController(SellerService sellerService, DepartmentService departmentService)
         {
             _sellerService = sellerService;
+            _departmentService = departmentService;
         }
         public IActionResult Index()
         {
@@ -19,7 +21,9 @@ namespace SaleWebMvc.Controllers
         }
         public IActionResult Create()
         {
-            return View();
+            var departments = _departmentService.FindAll();
+            var viewModel = new SellerFormViewModel { Departments = departments };
+            return View(viewModel);
         }
         [HttpPost]
         [ValidateAntiForgeryToken]
---
+++ SaleWebMvc/Services/DepartmentService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using SaleWebMvc.Models;
+using SaleWebMvc.Data;
+namespace SaleWebMvc.Services
+{
+    public class DepartmentService
+    {
+        public readonly SaleWebMvcContext _context;
+        public DepartmentService(SaleWebMvcContext context)
+        {
+            _context = context; 
+        }
+        public List<Department> FindAll()
+        {
+            return _context.Department.OrderBy(x => x.Name).ToList();
+        }
+    }
+}
